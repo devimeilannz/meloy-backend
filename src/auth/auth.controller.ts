b3ca51e1c740103459a4c8a650b9c3a919e883b8
devimeilannz <devimeilannilnanz@gmail.com>
2026-06-02 19:44:47 +0700

    add swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,8 @@ import {
 import { AuthService }
 from './auth.service';
 import { JwtAuthGuard } from 'src/helper/jwt-auth.guard';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,15 +19,15 @@ export class AuthController {
   constructor(
     private authService: AuthService,
   ) {}
-
-  @Post('register')
-  register(@Body() body: any) {
-
-    return this.authService.register(body);
-  }
+@Post('register')
+register(
+  @Body() body: RegisterDto,
+) {
+  return this.authService.register(body);
+}
 
   @Post('login')
-login(@Body() body: any) {
+login(@Body() body: LoginDto) {
   return this.authService.login(body);
 }
 
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -3,18 +3,24 @@ import {
   IsNotEmpty,
   IsOptional,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 
 export class RegisterDto {
 
+  @ApiProperty()
   @IsNotEmpty()
   username!: string;
 
+  @ApiProperty()
   @IsEmail()
   email!: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password!: string;
 
+  @ApiProperty()
   @IsOptional()
   role!: 'SUPER_ADMIN' | 'CUSTOMER';
 }
